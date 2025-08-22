0be38fd3e69f884ce005c42946819a77f5c1f515
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-08-22 09:53:54 -0600

    Función de suma de dos numeros

---
+++ funciones.py
@@ -0,0 +1,15 @@
+"""
+numero = int(input("Ingrese un numero: "))
+
+def cuadrado(num):
+    return num**2
+
+print(f"El cuadrado de {numero} es {cuadrado(numero)}")
+"""
+num1 = int(input("Ingrese el primer valor: "))
+num2 = int(input("Ingrese el primer valor: "))
+
+def suma(num1, num2):
+    return num1+num2
+#resul = suma(num1, num2)
+print(f"El resultado de sumar {num1} + {num2} es {suma(num1, num2)}")
