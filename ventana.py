a4928dff0264e9e26f834e0134d191fd45ee7d30
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-08-29 09:02:33 -0600

    Formulario base

---
+++ ventana.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+class aplicacion:
+    def __init__(self):
+        formulario = tk.Tk()
+        formulario.title("Alexis")
+        # Para que se muestre
+        formulario.mainloop()
+
+llamada = aplicacion()
+
+
